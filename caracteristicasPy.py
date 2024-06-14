402f3f94295f620812d19a966d766a7ca6800aa5
Marina79ok <toledomarina14@gmail.com>
2024-06-14 13:48:46 -0300

    Create caracteristicasPy.py

---
+++ caracteristicasPy.py
@@ -0,0 +1,22 @@
+'''
+Lenguaje de programación
+**Sintaxis en Python**
+La sintaxis en un lenguaje de programación se refiere a las reglas que 
+definen la estructura correcta de las secuencias de símbolos y palabras clave 
+que son legalmente correctas en ese lenguaje. Es análoga a la gramática en 
+los idiomas humanos.
+Por ejemplo, la correcta indentación, el uso de dos puntos (:) para iniciar 
+bloques de código y la sintaxis específica para definir funciones.
+Si un programador no sigue estas reglas sintácticas, el código no se ejecutará 
+correctamente. En el caso de Python, los errores de sintaxis se pueden 
+detectar durante la escritura del código gracias a las ayudas del editor.
+**Semántica en Python**
+La semántica se refiere al significado de las construcciones sintácticas. No es 
+suficiente que un programa esté sintácticamente correcto; las instrucciones 
+también deben tener sentido lógico para realizar la tarea deseada. La 
+semántica define cómo las estructuras sintácticas se traducen en 
+operaciones que se van a ejecutar en la máquina. 
+La semántica de Python describe cómo la ejecución del código resulta en un 
+comportamiento específico. 
+Un programa bien escrito debe ser tanto sintáctica como semánticamente 
+correcto para funcionar como se espera.
